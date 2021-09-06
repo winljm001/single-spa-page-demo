afa91f15a8146b3989f8d5ef1ba022ce4cd7d597
winljm001 <819648983@qq.com>
2021-09-06 15:08:11 +0800

    feat: add sh

--- footer-project/src/App.tsx
+++ footer-project/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">footer（react应用）</div>;
+  return <div className="footer">footer（react应用）</div>;
 }
 
 export default App;
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd base&&yarn start & cd header-project&&yarn start & cd side-project&&yarn start & cd footer-project&&yarn start & cd content-project&&yarn serve
